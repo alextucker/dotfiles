3423d08d6a8b785293f9aa8a3b88ed006f8014fe
Alex Tucker <tucker.al@gmail.com>
2013-10-06 13:44:45 -0400

    Consolidating apt-gets and forcing yes

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,12 @@
 
 # Install base apt packages
 sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install git-core
-sudo apt-get install vim
-sudo apt-get install tmux
-sudo apt-get install zsh
+sudo apt-get install -y build-essential git-core vim tmus zsh python-pip python-dev
 
 # Install Ruby 1.9.3 from Source
 wget http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p448.tar.gz
 tar -zxvf ruby-1.9.3-p448.tar.gz
-cd ruby-1.9.3-p448.tar.gz
+cd ruby-1.9.3-p448
 ./configure
 make
 sudo make install
@@ -20,14 +16,13 @@ cd ..
 # Install NodeJS 0.10.x from Source
 wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
 tar -zxvf node-v0.10.20.tar.gz
-cd node-v0.10.20.tar.gz
+cd node-v0.10.20
 ./configure
 make
 sudo make install
 cd ..
 
 # Install some global python stuff
-sudo easy_install pip
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 sudo pip install grip
