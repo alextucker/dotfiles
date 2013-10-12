df7409e3720d6f377bf060ea4153d7a44b180500
Alex Tucker <tucker.al@gmail.com>
2013-10-12 16:46:39 -0400

    Improving

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,26 +22,30 @@ fi
 
 if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
     git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+    rbenv install 1.9.3-p392
+    rbenv global 1.9.3-p392
 fi
 
-rbenv install 1.9.3-p392
-rbenv global 1.9.3-p392
 
 # Install NodeJS 0.10.x from Source
-wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
-tar -zxvf node-v0.10.20.tar.gz
-cd node-v0.10.20
-./configure
-make
-sudo make install
-cd ..
+if ! hash node 2>/dev/null; then
+    wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz
+    tar -zxvf node-v0.10.20.tar.gz
+    cd node-v0.10.20
+    ./configure
+    make
+    sudo make install
+    cd ..
+fi
 
 # Install some global python stuff
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.zshrc
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
-sudo pip install grip
+if ! hash workon 2>/dev/null; then
+    sudo pip install virtualenv
+    sudo pip install virtualenvwrapper
+    echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.zshrc
+    echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+    sudo pip install grip
+fi
 
 # Install some global node stuff
 sudo npm install -g grunt-cli
@@ -63,8 +67,10 @@ if [ ! -d ~/.gitflow ]; then
     git clone https://github.com/nvie/gitflow.git ~/.gitflow
     cd ~/.gitflow
     git submodule init && git submodule update
-    make install
+    sudo make install
     export PATH="$HOME/.gitflow:$PATH"
+    echo 'export PATH="$HOME/.gitflow:$PATH"' >> ~/.bashrc
+    echo 'export PATH="$HOME/.gitflow:$PATH"' >> ~/.zshrc
 fi
 
 # Make zsh default shell
